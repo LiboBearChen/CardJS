4ddbd8579c69793e3b31ca4b9b965ebac4515fe2
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-04-16 17:25:22 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 import Deck from "./deck.js";
 const computerCardSlot = document.querySelector(".computer-card-slot");
-const deck = new Deck();
-deck.shuffle();
-computerCardSlot.appendChild(deck.cards[0].getHTML());
+const playerCardSlot = document.querySelector(".player-card-slot");
+const computerDeckElement = document.querySelector(".computer-deck");
+const playerDeckElement = document.querySelector(".player-deck");
+const text = document.querySelector(".text");
+
+let playerDeck, computerDeck;
+
+startGame();
+function startGame() {
+  const deck = new Deck();
+  deck.shuffle();
+  const deckMdipoint = Math.ceil(deck.nnumberOfCards / 2);
+  playerDeck = new Deck(deck.cards.slice(0, deckMdipoint));
+  computerDeck = new Deck(deck.cards.slice(deckMdipoint, deck.numberOfCards));
+
+  cleanBeforeRound();
+}
+
+function cleanBeforeRound() {
+  computerCardSlot.innerHTML = "";
+  playerCardSlot.innerHTML = "";
+  text.innerHTML = "";
+
+  updateDeckCount();
+}
+
+function updateDeckCount() {}
